4ad910ef9217b95da6ec09ca7f87aa2ebbb2c576
premg513 <gunduboinaprem@gmail.com>
2026-05-11 13:44:09 +0530

    visble when hover

--- Task-A/app/page.tsx
+++ Task-A/app/page.tsx
@@ -1,24 +1,19 @@
-'use client';
+"use client";
 
-import BlurText from '@/components/BlurText';
-import MagnetLines from '@/components/MagnetLines';
+import BlurText from "@/components/BlurText";
+import MagnetLines from "@/components/MagnetLines";
 
 export default function Home() {
   const handleAnimationComplete = () => {
-    console.log('Animation completed!');
+    console.log("Animation completed!");
   };
 
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center gap-20 p-10">
-      <BlurText
-        text="Magnetic UI Animation"
-        delay={150}
-        animateBy="words"
-        direction="top"
-        onAnimationComplete={handleAnimationComplete}
-        className="text-5xl font-bold text-center"
+        <BlurText
+        text="Hover over this entire sentence"
+        className="text-6xl font-bold text-white"
       />
-
       <MagnetLines
         rows={10}
         columns={12}
@@ -27,7 +22,7 @@ export default function Home() {
         lineWidth="2px"
         lineHeight="30px"
         baseAngle={-10}
-        style={{ margin: '2rem auto' }}
+        style={{ margin: "2rem auto" }}
       />
     </main>
   );
